71ba14612b002f620a398ccb1deaf337bf12a7f0
Thiago Lanza <lanzathm@gmail.com>
2020-02-24 15:22:46 -0300

    Module exports

--- data.js
+++ data.js
@@ -3,19 +3,19 @@ module.exports = [
     {
         id: 'starter', 
         id_img: '64c237ccff807c054339a62d53b4b402',
-        modules: '6 Módulos',
-        price: 'Free'
+        modules: '5 Módulos',
+        price: 'Gratuito'
     },
     {
         id: 'launchbase',
         id_img: '0828532024cb46921a6b5e941f8d788d',
-        modules: '6 Módulos',
-        price: 'Paid'
+        modules: '13 Módulos',
+        price: 'Pago'
     },
     {
         id: 'gostack',
         id_img: '83a178a0653dab1d55e2ed7946465975',
-        modules: '6 Módulos',
-        price: 'Paid'
+        modules: '27 Módulos',
+        price: 'Pago'
     }
 ]
