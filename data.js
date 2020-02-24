db4b7623a9541ebc54a2ca84c5b6318b5812830c
Thiago Lanza <lanzathm@gmail.com>
2020-02-23 22:11:18 -0300

    module exports

---
+++ data.js
@@ -0,0 +1,21 @@
+//conteúdo a ser exportado para utilizar na rota /courses
+module.exports = [
+    {
+        id: 'starter', 
+        id_img: '64c237ccff807c054339a62d53b4b402',
+        modules: '6 Módulos',
+        price: 'Free'
+    },
+    {
+        id: 'launchbase',
+        id_img: '0828532024cb46921a6b5e941f8d788d',
+        modules: '6 Módulos',
+        price: 'Paid'
+    },
+    {
+        id: 'gostack',
+        id_img: '83a178a0653dab1d55e2ed7946465975',
+        modules: '6 Módulos',
+        price: 'Paid'
+    }
+]
